5097ca008a93ddb2c6eebb4312af09c9515207ed
Velasco Campos Cristian Daniel <21151014@aguascalientes.tecnm.mx>
2024-05-27 20:07:42 -0600

    cris

--- ProyectoFinalU4/proyecto-laravel/resources/views/clientes/update.blade.php
+++ ProyectoFinalU4/proyecto-laravel/resources/views/clientes/update.blade.php
@@ -68,6 +68,141 @@
             </div>
         </div>
     </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('#formulario').submit(function (event) {
+                event.preventDefault();
+
+                var constraints = {
+                    name: {
+                        presence: true,
+                        length: {
+                            minimum: 1,
+                            message: "Ingrese un nombre válido"
+                        }
+                    },
+                    apellido: {
+                        presence: true,
+                        length: {
+                            minimum: 1,
+                            message: "Ingrese un apellido válido"
+                        }
+                    },
+                    telefono: {
+                        presence: true,
+                        numericality: {
+                            onlyInteger: true,
+                            message: "Ingrese solo números"
+                        },
+                        length: {
+                            is: 10,
+                            message: "El teléfono debe tener 10 dígitos"
+                        }
+                    },
+                    @if(Auth::user()->role == 1)
+                        role: {
+                                presence: true,
+                                length: {
+                                    minimum: 1,
+                                    message: "Ingrese un rol válido"
+                                }
+                            },
+                    @endif
+                @if(Auth::user()->role != 1)
+                    password: {
+                            presence: true,
+                            length: {
+                                minimum: 8,
+                                message: "La contraseña debe tener al menos 8 caracteres"
+                            }
+                        },
+                @endif
+                fechaNacim: {
+                        presence: true,
+                        date: {
+                            latest: new Date(new Date().setFullYear(new Date().getFullYear() - 18)),
+                            message: "Debe ser mayor de 18 años"
+                        }
+                    },
+                    email: {
+                        presence: true,
+                        email: {
+                            message: "No es un formato de correo electrónico válido"
+                        },
+                        format: {
+                            pattern: /^[^0-9][a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Debe ser un correo electrónico válido"
+                        }
+                    }
+                };
+
+                var formData = {
+                    name: $('#nombre').val(),
+                    apellido: $('#apellido').val(),
+                    telefono: $('#telefono').val(),
+                    @if(Auth::user()->role == 1)
+                        role: $('#rol').val(),
+                    @endif
+                @if(Auth::user()->role != 1)
+                    password: $('#password').val(),
+                @endif
+                fechaNacim: $('#fechaNacim').val(),
+                    email: $('#email').val()
+                };
+
+                var errors = validate(formData, constraints);
+                if (errors) {
+                    var errorMessage = '';
+                    if (errors.name) {
+                        errorMessage += errors.name.join('<br>');
+                    }
+                    if (errors.apellido) {
+                        errorMessage += '<br>' + errors.apellido.join('<br>');
+                    }
+                    if (errors.telefono) {
+                        errorMessage += '<br>' + errors.telefono.join('<br>');
+                    }
+                    if (errors.fechaNacim) {
+                        errorMessage += '<br>' + errors.fechaNacim.join('<br>');
+                    }
+                    if (errors.email) {
+                        errorMessage += '<br>' + errors.email.join('<br>');
+                    }
+                    @if(Auth::user()->role == 1)
+                        if (errors.role) {
+                            errorMessage += '<br>' + errors.role.join('<br>');
+                        }
+                    @endif
+                    @if(Auth::user()->role != 1)
+                        if (errors.password) {
+                            errorMessage += '<br>' + errors.password.join('<br>');
+                        }
+                    @endif
+                    $('#mensaje-error').html(errorMessage);
+                } else {
+                    $('#mensaje-error').html('');
+                    alert('Formulario enviado correctamente');
+                    $('#nombre').val('');
+                    $('#apellido').val('');
+                    $('#telefono').val('');
+                    @if(Auth::user()->role == 1)
+                        $('#rol').val('');
+                    @endif
+                    @if(Auth::user()->role != 1)
+                        $('#password').val('');
+                    @endif
+                    $('#fechaNacim').val('');
+                    $('#email').val('');
+                }
+            });
+        });
+    </script>
+
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap JS, Popper.js, and jQuery -->
